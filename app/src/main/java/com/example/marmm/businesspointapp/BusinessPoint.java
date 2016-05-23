22e49597e6e211fad94ca9dd9100192bed56cfb6
Marcellis <m.m.c.m.marcellis@hva.nl>
2016-05-23 19:24:54 +0200

    Initial commit

---
+++ app/src/main/java/com/example/marmm/businesspointapp/BusinessPoint.java
@@ -0,0 +1,45 @@
+package com.example.marmm.businesspointapp;
+
+/**
+ * Created by marmm on 5/22/16.
+ */
+public class BusinessPoint {
+
+    private String name;
+    private  String teacher;
+    private  int credits;
+    private  double grade;
+
+
+    public String getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(String teacher) {
+        this.teacher = teacher;
+    }
+
+    public int getCredits() {
+        return credits;
+    }
+
+    public void setCredits(int credits) {
+        this.credits = credits;
+    }
+
+    public double getGrade() {
+        return grade;
+    }
+
+    public void setGrade(double grade) {
+        this.grade = grade;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
